d6a48d23508930e1de75e3ce9861dad3f18a842c
Ashwin Natraj <ashwinnatraj97@gmail.com>
2022-05-24 00:27:13 -0400

    Added Header and Footer in Lib

---
+++ src/lib/footer.svelte
@@ -0,0 +1,12 @@
+<div class="footer">
+    <!-- <a class="link link-primary" href="/">Home</a> -->
+    <p>Copyright &copy; 2022 OATS</p>
+</div>
+
+
+<style>
+    .footer{
+        width: 100%;
+        text-align: center;
+    }
+</style>
---
+++ src/lib/title.svelte
@@ -0,0 +1,18 @@
+<script>
+    import '../app.css'
+</script>
+
+<div class="title">
+    <img src="IoT4Ag.svg" alt="IoT4Ag-logo" width="70%">
+</div>
+
+<style>
+    .title{
+        width: 100%;
+        text-align: center;
+    }
+    .title img{
+        display: inline-block;
+        margin: 0 auto;
+    }
+</style>
