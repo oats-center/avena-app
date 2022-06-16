4984391eac8f84de2be3882310bdbaeda52ffc2d
Andrew Balmos <andrew@balmos.org>
2022-06-16 02:55:30 -0400

    Use static adapter
    Signed-off-by: Andrew Balmos <andrew@balmos.org>

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { isoImport } from 'vite-plugin-iso-import';
 
